fa6ffece381977dd4d83717a236dfb4829874c86
Ghandourism <mostafahany5@hotmail.com>
2023-07-16 02:04:28 +0300

    website with app. problem

--- Website/app.py
+++ Website/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+from views import views
 app=Flask(__name__)
 
 @app.route("/")
+app.register_blueprint(views,url_prefix="/")
+
 def home():
     return "home page"
 if __name__=='__main__':
