6ce832160bd49f98dc5b3cca266194521105d135
Ghandourism <mostafahany5@hotmail.com>
2023-07-17 06:31:25 +0300

    test 1

--- Website/app.py
+++ Website/app.py
@@ -1,12 +1,8 @@
 from flask import Flask
 from views import views
 app=Flask(__name__)
+app.register_blueprint(views,url_prefix="/views")
 
-@app.route("/")
-app.register_blueprint(views,url_prefix="/")
-
-def home():
-    return "home page"
 if __name__=='__main__':
     app.run(debug=True,port=8000)
 
--- Website/views.py
+++ Website/views.py
@@ -5,6 +5,3 @@ views = Blueprint(__name__,"views")
 @views.route("/")
 def home():
     return render_template("index.html")  
-@views.route("/home")
-def home():
-    return "home page"
